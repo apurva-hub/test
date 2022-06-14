49ad679eb321f5fe1c371840d9b4af8c98ca8c66
L8Y <madebyhuman@outlook.com>
2022-06-14 19:32:40 +0530

    Add project files.

---
+++ WebApplication3/Controllers/HomeController1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public ViewResult login()
+        {
+            int[] a = { 1, 2, 3, 4 };
+            ViewData["data"] = a;
+            return View();  
+        }
+
+    }
+}
